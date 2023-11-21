02e7a1aefd6c2f73dee1ed7f551e0246e6c95513
Ngoc Duy Tran <duyliken2@gmail.com>
2023-11-22 00:09:44 +0700

    add terraform

---
+++ terraform/input.tf
@@ -0,0 +1,24 @@
+# Azure GUIDS
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+# Resource Group/Location
+variable "location" {}
+variable "resource_group" {}
+variable "application_type" {}
+
+# Network
+variable virtual_network_name {}
+variable address_prefix_test {}
+variable address_space {}
+
+# VM
+variable "admin_username" {}
+variable "admin_password" {
+    default = "tQ=~cGqN;z[(,6:3"
+}
+
+variable "public_key_path" {}
+
