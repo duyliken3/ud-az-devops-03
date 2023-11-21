22f316796ab35e7b1cb2dedf33b99a66a0c30549
Ngoc Duy Tran <duyliken2@gmail.com>
2023-11-21 21:25:31 +0700

    update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -4,6 +4,8 @@ variable "resource_group" {}
 variable "prefix" {}
 variable "public_ip_address_id" {}
 variable "subnet_id" {}
-variable "vm_password" {}
+variable "vm_password" {
+    default = "th3_flatw0rld"
+}
 variable "admin_username" {}
 variable "public_key_path" {}
