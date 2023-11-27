1d0b90dc78a0ea6ebbc4cc807a69b0de762adfa6
Ngoc Duy Tran <duyliken2@gmail.com>
2023-11-27 23:58:55 +0700

    update

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -64,6 +64,9 @@ logging.info (current_time() + 'Starting the browser...')
 # --uncomment when running in Azure DevOps.
 options = ChromeOptions()
 options.add_argument("--headless") 
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(options=options)
 # driver = webdriver.Chrome()
 
